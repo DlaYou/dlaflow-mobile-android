87b6c56869e69e294418adb3fe16a69923664842
MaciekJRPL <MaciekJRPL@gmail.com>
2026-08-20 17:56:02 +0200

    release: prepare Android Mobile Assistant 0.4.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.dlaflow.mobile"
         minSdk = 28
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.4.8"
+        versionCode = 23
+        versionName = "0.4.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["usesCleartextTraffic"] = "false"
     }
