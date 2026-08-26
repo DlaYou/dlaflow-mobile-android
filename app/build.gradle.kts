c074efc16a0ab4a92f46fc45a9e711aa91785b6b
MaciekJRPL <MaciekJRPL@gmail.com>
2026-08-26 20:31:18 +0200

    release: prepare Mobile Assistant 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.dlaflow.mobile"
         minSdk = 28
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.5.0"
+        versionCode = 26
+        versionName = "0.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["usesCleartextTraffic"] = "false"
     }
